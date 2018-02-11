c4b5fcda8913880f180ace579e100b476e3e083a
Amos Gwa <amosgwa@gmail.com>
2018-02-11 00:49:28 -0700

    Keyboard boilerplate is working now

--- app/src/main/java/com/amosgwa/lisukeyboard/LISUKeyboard.kt
+++ app/src/main/java/com/amosgwa/lisukeyboard/LISUKeyboard.kt
@@ -1,13 +1,18 @@
 package com.amosgwa.lisukeyboard
 
+import android.content.Context
 import android.inputmethodservice.InputMethodService
 import android.inputmethodservice.Keyboard
 import android.inputmethodservice.KeyboardView
+import android.media.AudioManager
+import android.view.KeyEvent
 import android.view.View
 
 class LISUKeyboard : InputMethodService(), KeyboardView.OnKeyboardActionListener {
-    var keyboardView : KeyboardView? = null
-    var keyboard : Keyboard? = null
+    private var keyboardView: KeyboardView? = null
+    private var keyboard: Keyboard? = null
+
+    private var isCaps = false
 
     override fun onCreateInputView(): View? {
         keyboardView = layoutInflater.inflate(R.layout.keyboard, null) as KeyboardView?;
@@ -19,34 +24,58 @@ class LISUKeyboard : InputMethodService(), KeyboardView.OnKeyboardActionListener
     }
 
     override fun swipeRight() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onPress(primaryCode: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onRelease(primaryCode: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun swipeLeft() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun swipeUp() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun swipeDown() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
     override fun onKey(primaryCode: Int, keyCodes: IntArray?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val inputConnection = currentInputConnection
+        playClick(primaryCode)
+        when (primaryCode) {
+            Keyboard.KEYCODE_DELETE -> {
+                inputConnection.deleteSurroundingText(1, 0)
+            }
+            Keyboard.KEYCODE_SHIFT -> {
+                isCaps = !isCaps
+                keyboard?.isShifted = isCaps
+                keyboardView?.invalidateAllKeys()
+            }
+            Keyboard.KEYCODE_DONE -> {
+                inputConnection.sendKeyEvent(KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER))
+            }
+            else -> {
+                var code = primaryCode.toChar()
+                if (Character.isLetter(code) && isCaps) {
+                    code = Character.toUpperCase(code)
+                }
+                inputConnection.commitText(code.toString(), 1)
+            }
+        }
+    }
+
+    private fun playClick(keyCode: Int) {
+        val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        when (keyCode) {
+            32 -> audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR)
+            Keyboard.KEYCODE_DONE, 10 -> audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN)
+            Keyboard.KEYCODE_DELETE -> audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE)
+            else -> audioManager.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD)
+        }
     }
 
     override fun onText(text: CharSequence?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
