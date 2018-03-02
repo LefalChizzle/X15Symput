acb702bb9a06a6dc67c078996e1564ae59eb6476
Amos Gwa <amosgwa@gmail.com>
2018-03-01 23:59:26 -0700

    Added subscript
    Added new keys for symbols
    
    Added landscape height for the keyboard

--- app/src/main/java/com/amosgwa/lisukeyboard/Amos/GwaKeyboardView.kt
+++ app/src/main/java/com/amosgwa/lisukeyboard/Amos/GwaKeyboardView.kt
@@ -8,6 +8,10 @@ import android.inputmethodservice.KeyboardView
 import android.util.AttributeSet
 import android.view.MotionEvent
 import com.amosgwa.lisukeyboard.R
+import android.R.attr.y
+import android.R.attr.x
+
+
 
 /**
  * Created by Amos Gwa on 2/18/2018.
@@ -32,31 +36,41 @@ class GwaKeyboardView @JvmOverloads constructor(
         return super.onTouchEvent(me)
     }
 
-    private fun drawPopup(){
+    private fun drawPopup() {
 
     }
 
     private fun drawCustomKeys(canvas: Canvas) {
         if (keyboard != null) {
-            val paint = Paint()
-            paint.textAlign = Paint.Align.CENTER
-            paint.textSize = 25f
-            paint.color = Color.RED
+//            val paint = Paint()
+//            paint.textAlign = Paint.Align.CENTER
+//            paint.textSize = 25f
+//            paint.color = Color.RED
 
-            for (i in 0 until keyboard.keys.size) {
-                for (key in keyboard.keys) {
-                    val key = keyboard.keys[i]
-                    if (key.codes.isNotEmpty() && key.codes[0] == KEYCODE_SPACE) {
-                        val dr = resources.getDrawable(R.drawable.ic_space)
-                        val width = Math.round(key.width * 0.8).toInt()
-                        val height = Math.round(key.height * 0.2).toInt()
-                        val startX = (key.width - width) / 2 + key.x
-                        val startY = (key.height - height) / 2 + (key.y * 1.03).toInt()
-                        val endX = startX + width
-                        val endY = startY + height
-                        dr.setBounds(startX, startY, endX, endY)
-                        dr.draw(canvas)
-                    }
+            for (key in keyboard.keys) {
+                if(key.label != null) {
+                    val subKeyPaint = Paint()
+//                    canvas.drawPaint(subKeyPaint)
+                    subKeyPaint.color = Color.RED
+//                    subKeyPaint.textAlign = Paint.Align.CENTER
+                    subKeyPaint.textSize = 26f
+                    val paddingX = key.width * 0.2
+                    val paddingY = key.height * 0.2
+                    val startX = (key.width + key.x - paddingX).toFloat()
+                    val startY = (paddingY + key.y).toFloat()
+//                    canvas.drawText(key.label as String?, startX, startY, subKeyPaint)
+                }
+
+                if (key.codes.isNotEmpty() && key.codes[0] == KEYCODE_SPACE) {
+                    val dr = resources.getDrawable(R.drawable.ic_space)
+                    val width = Math.round(key.width * 0.8).toInt()
+                    val height = Math.round(key.height * 0.2).toInt()
+                    val startX = (key.width - width) / 2 + key.x
+                    val startY = (key.height - height) / 2 + (key.y * 1.03).toInt()
+                    val endX = startX + width
+                    val endY = startY + height
+                    dr.setBounds(startX, startY, endX, endY)
+                    dr.draw(canvas)
                 }
             }
         }
